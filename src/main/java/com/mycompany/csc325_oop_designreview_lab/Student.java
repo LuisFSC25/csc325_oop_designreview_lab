2d11ff164d986ace1df5df08f01c198ff4ad6dcf
Luis Colocho Escobar <144961859+LuisFSC25@users.noreply.github.com>
2024-04-08 21:20:51 -0400

    Update Student.java

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,12 +8,22 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Human
-
-	// ToDo 2: Fix the resulting errors
-
-	// ToDo 3: Add a field for GPA and create a setter and a getter
+public class Student extends Human { // ToDo 1: Make this class a child of Human
 	
+	// ToDo 2: Fix the resulting errors
+	 private double gpa;
+	// ToDo 3: Add a field for GPA and create a setter and a getter
+	//Constructor
+	 public Student(String name, short age) {
+        super(name, age); // Calling the parent constructor
+    }
+ // getter for GPA
+	public double getGpa() {  // ToDo 3: Getter for GPA
+        return gpa;
+    }
+   //setter for GPA
+	public void setGpa(double gpa) { // ToDo 3: Setter for GPA
+        this.gpa = gpa;
+    }
 	// ToDo 4: Add comments to your code
 }
